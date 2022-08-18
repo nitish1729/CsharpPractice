589a8b4a5181fe36a2220ebeb92c503235c51d63
Nitish Mehta <nkrmehta09@gmail.com>
2022-08-18 14:27:11 +0530

    PracticeSection

---
+++ CsharpPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using CsharpPractice;
+
+
+
+
+Console.WriteLine("Hello, World!");
+
+//CodingPractice code = new CodingPractice();
+//code.prime();
+//code.prime1();
+//code.Armstrong();
+
+ArrayPractice p1 = new ArrayPractice();
+//p1.Arrays();
+//int[] Arr1 = new int[] { 2, 4, 6, 8, 10, 12 };
+//int[] Arr3 = new int[5] {5,10,15,20,25 };
+//p1.ArryFunction(Arr3);
+//p1.ArryFunction(Arr1);
+//p1.printMin(Arr3);
+//p1.MultiDArray();
+//p1.Arrayclass();
+// Command line arguments  
+//Console.WriteLine("Argument length: " + args.Length);
+//Console.WriteLine("Supplied Arguments are:");
+//foreach (Object obj in args)
+//{ 
+//    Console.WriteLine(obj);
+//}
+CollectionList l1 = new CollectionList();
+l1.CList();
